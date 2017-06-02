adcf6ca06fcc202767c72ca194bfcc01ba054f19
Teun van Gils <edu@teunvg.nl>
2017-06-02 11:34:33 +0200

    Made movie picker code more readable.

--- project.py
+++ project.py
@@ -1,13 +1,20 @@
+'''
+    This script picks a random movie from a user-specified list of movies.
+'''
 from random import shuffle
 
-def my_funct():
-    my_list = []
-    for i in range(text1):
-        text = input("What is your %dst? " % (i + 1))
-        my_list.append(text)
-    shuffle(my_list)
-    return my_list
-text1 = int(input("How many do you want to choose between? "))
+def input_list(n_items):
+    '''This function asks for a given number of items and returns a list of these items. '''
+    items = []
+    for i in range(n_items):
+        user_input = input("What is your %dst? " % (i + 1))
+        items.append(user_input)
+    return items
 
-list_2 = my_funct()
-print(list_2[0])
+# Ask the user for a specified number of movies
+n_movies = int(input("How many movies do you want to choose between? "))
+movies = input_list(n_movies)
+shuffle(movies)
+
+# Print the first element of the shuffled list of movies
+print(movies[0])
